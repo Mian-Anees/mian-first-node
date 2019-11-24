a5da329bab7775139b5ab2a0f293ba9f14c71c79
mian anees <miananees23@gmail.com>
2019-11-24 11:07:13 +0500

    commit 11/24

--- expres.js
+++ expres.js
@@ -18,21 +18,21 @@ app.get('/',(req,res)=>{
     
    });
 app.get('/api/:id/:id2', (req,res,next)=>{
-const id1=req.params.id;
- const id2=req.params.id2;
- const db ='mongodb+srv://Miananees:Mian12345@gettingstarted-jtf43.mongodb.net/test?retryWrites=true&w=majority';
- mongoose.set('useUnifiedTopology',true);
- mongoose.connect(db,{'useNewUrlParser':true,'useCreateIndex':true}).then(()=>console.log('conect to db'))
- .catch((err)=>{console.log(err)});
-const NewUser=new schema(
+     const id1=req.params.id;
+      const id2=req.params.id2;
+      const db ='mongodb+srv://Miananees:Mian12345@gettingstarted-jtf43.mongodb.net/test?retryWrites=true&w=majority';
+      mongoose.set('useUnifiedTopology',true);
+      mongoose.connect(db,{'useNewUrlParser':true,'useCreateIndex':true}).then(()=>console.log('conect to db'))
+     .catch((err)=>{console.log(err)});
+     const NewUser=new schema(
    {
       name: id1,
       pascode:id2
    }
 );
 NewUser.save().then((NewUser)=>{
-   console.log('Datasave'+NewUser);
-   res.send('okkk');
+     console.log('Datasave'+NewUser);
+       res.send('okkk');
 }).catch((err)=>{console.log(err)});
 });
 app.get('/Login/:id/:id',(req,res,next)=>{
